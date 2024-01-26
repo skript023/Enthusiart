a675a9a926add3a441e611a5c79cd5532254fe99
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-01-26 17:12:03 +0700

    add database migration

---
+++ database/migrations/2024_01_26_094358_create_galleries_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->string('image');
+            $table->string('artwork_name');
+            $table->string('artist_name');
+            $table->string('materials');
+            $table->string('dimension');
+            $table->string('description');
+            $table->string('year');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreign('category_id')->references('id')->on('categories')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('galleries');
+    }
+};
