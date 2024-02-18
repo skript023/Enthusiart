b3625230a7299a71144d55a3e3702ef8d4b7f743
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-02-18 14:19:01 +0700

    add favorite

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -4,7 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\favorite;
+
 class FavoriteController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $request->validate([
+            'gallery_id' => 'required'
+        ]);
+
+        $data = $request->only([
+            'gallery_id'
+        ]);
+
+        try 
+        {
+            favorite::create($data);
+
+            return back();
+        } 
+        catch (\Throwable $th) 
+        {
+            return redirect()->intended('/');
+        }
+    }
 }
