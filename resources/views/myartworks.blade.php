6e804b3325b1f71f2a4e3d8c1f72e806c095e513
nolafarah <nfarah017@gmail.com>
2024-03-01 12:55:37 +0700

    add myartworks views

---
+++ resources/views/myartworks.blade.php
@@ -0,0 +1,67 @@
+@extends('template.template')
+@section('content')
+<section class="py-5 my-artworks mb-5" style="margin-top: 80px;">
+    <div class="container">
+        <h1 class="page-title text-center">My Artworks</h1>
+        <div class="btn-wrapper d-flex justify-content-end">
+            <a href="upload-artwork.html" class="btn btn-upload" style="margin: 8px 0px;">Upload Artwork</a>
+        </div>
+        <!-- Card Section -->
+        <div class="row d-flex">
+            <div class="card-artwork">
+                <div class="d-flex justify-content-between">
+                    <h2 class="artist-name">Raden Saleh</h2>
+                    <div class="icon-wrapper">
+                        <i class="fa-solid fa-ellipsis" id="ellipsis-icon" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                        <div class="dropdown-menu" aria-labelledby="ellipsis-icon">
+                            <a href="#" class="dropdown-item delete-option">Delete</a>
+                        </div>
+                    </div>
+                </div>
+                <img src="../assets/img/art-gallery.jpg" class="card-img-top" alt="Art Nouveau">
+                <div class="card-body">
+                    <h2 class="art-title">Perburuan Banteng</h2>
+                    <p class="card-desc">Acrylic on Canvas<br>110 x 80 cm</p>
+                    <a href="#" class="card-link">View Details</a>
+                </div>
+            </div>
+
+            <div class="card-artwork">
+                <div class="d-flex justify-content-between">
+                    <h2 class="artist-name">Raden Saleh</h2>
+                    <div class="icon-wrapper">
+                        <i class="fa-solid fa-ellipsis" id="ellipsis-icon" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                        <div class="dropdown-menu" aria-labelledby="ellipsis-icon">
+                            <a href="#" class="dropdown-item delete-option">Delete</a>
+                        </div>
+                    </div>
+                </div>
+                <img src="../assets/img/art-gallery.jpg" class="card-img-top" alt="Art Nouveau">
+                <div class="card-body">
+                    <h2 class="art-title">Perburuan Banteng</h2>
+                    <p class="card-desc">Acrylic on Canvas<br>110 x 80 cm</p>
+                    <a href="#" class="card-link">View Details</a>
+                </div>
+            </div>
+
+            <div class="card-artwork">
+                <div class="d-flex justify-content-between">
+                    <h2 class="artist-name">Raden Saleh</h2>
+                    <div class="icon-wrapper">
+                        <i class="fa-solid fa-ellipsis" id="ellipsis-icon" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                        <div class="dropdown-menu" aria-labelledby="ellipsis-icon">
+                            <a href="#" class="dropdown-item delete-option">Delete</a>
+                        </div>
+                    </div>
+                </div>
+                <img src="../assets/img/art-gallery.jpg" class="card-img-top" alt="Art Nouveau">
+                <div class="card-body">
+                    <h2 class="art-title">Perburuan Banteng</h2>
+                    <p class="card-desc">Acrylic on Canvas<br>110 x 80 cm</p>
+                    <a href="#" class="card-link">View Details</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
