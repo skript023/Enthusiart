ab2f0e8009a21bc1f7d9ef24fe3ccd71a7014345
nolafarah <nfarah017@gmail.com>
2024-07-10 15:41:13 +0700

    update style search

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -6,6 +6,7 @@
         <!-- Search Section -->
         <div class="search-bar mb-4">
             <input type="text" id="search-artworks" class="form-control" placeholder="Search Artworks">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#7d7d7d" d="M5 10a5 5 0 1 1 10 0a5 5 0 0 1-10 0m5-7a7 7 0 1 0 4.192 12.606l5.1 5.101a1 1 0 0 0 1.415-1.414l-5.1-5.1A7 7 0 0 0 10 3"/></svg>
         </div>
         <!-- Card Section -->
         <div class="row d-flex">
