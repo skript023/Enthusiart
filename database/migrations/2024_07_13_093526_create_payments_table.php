54d01b579002306cd98557f66a9488e9acc8485a
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-07-13 22:39:42 +0700

    plan to add payment & transaction

---
+++ database/migrations/2024_07_13_093526_create_payments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id')->nullable();
+            $table->string('transaction_id');
+            $table->string('merchant_id');
+            $table->bigInteger('gross_amount');
+            $table->string('name');
+            $table->string('description');
+            $table->string('currency');
+            $table->string('payment_type');
+            $table->string('transaction_status');
+            $table->string('transaction_time');
+            $table->string('fraud_status');
+            $table->timestamp('expiry_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
